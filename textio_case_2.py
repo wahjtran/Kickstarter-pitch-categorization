7e0d94892defa0073e101a859c127a112e5d2884
Nick Tran <wjtran@ncsu.edu>
2017-10-05 10:23:20 -0700

    Preliminary pipeline established

--- textio_case_2.py
+++ textio_case_2.py
@@ -22,7 +22,7 @@ StopWords = stopwords.words('english')
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.decomposition import LatentDirichletAllocation
@@ -49,24 +49,92 @@ X = text['full_text']
 y = text['category']
 
 
+
 scores = []
 for i in range(10):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y)
     x = []
-    for j in range(1000,10001,500):
+    for j in [1000,2000,4000,8000,12000,16000,20000]:
+        
         classifier = Pipeline([
-            ('vectorizer', CountVectorizer(max_features=10000, analyzer='word', stop_words=StopWords)),
+            ('vectorizer', CountVectorizer(max_features=j, analyzer='word', stop_words=StopWords)),
             ('tfidf', TfidfTransformer()),
-            ('clf', LinearSVC(multi_class='crammer_singer'))])
+            ('clf', LinearSVC(multi_class='ovr'))]) 
+    
         classifier.fit(X_train, y_train)
         x.append(classifier.score(X_test, y_test))
-        #print(i,j)
+        print(i,j)
     scores.append(x)
+feat_scores = pd.DataFrame(scores, columns=[1000,2000,4000,8000,12000,16000,20000])
+plt.plot(feat_scores.mean())
+n_feat = 8000
+#feat_rng = range(5000, 10001, 500)
 
-feat_scores = pd.DataFrame(scores, columns=range(1000,10001,500))
-#plt.plot(feat_scores.mean())
+
+scores = []
+for i in range(10):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y)
+    x = []
+    for j in [10,20,30,50,100,200]:
+        
+        classifier = Pipeline([
+            ('vectorizer', CountVectorizer(max_features=n_feat, analyzer='word', stop_words=StopWords)),
+            ('lda', LatentDirichletAllocation(n_topics=j, n_jobs=-1)),
+            ('clf', LinearSVC(multi_class='ovr'))])
+    
+        classifier.fit(X_train, y_train)
+        x.append(classifier.score(X_test, y_test))
+        print(i,j)
+    scores.append(x)
+topic_scores = pd.DataFrame(scores, columns=[10,20,30,50,100,200])
+plt.plot(topic_scores.mean())
+test_topic = 
 
 
+scores = []
+for i in range(10):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y)
+    x = []
+    for j in [10,20,30,50,100,200]:
+        
+        classifier = Pipeline([
+            ('vectorizer', CountVectorizer(max_features=n_feat, analyzer='word', stop_words=StopWords)),
+            ('lda', LatentDirichletAllocation(n_topics=j, n_jobs=-1)),
+            ('clf', LinearSVC(multi_class='ovr'))])
+    
+        classifier.fit(X_train, y_train)
+        x.append(classifier.score(X_test, y_test))
+        print(i,j)
+    scores.append(x)
+topic_scores = pd.DataFrame(scores, columns=[10,20,30,50,100,200])
+plt.plot(topic_scores.mean())
+
+
+classifier = Pipeline([
+        ('vectorizer', CountVectorizer(max_features=test_feat, analyzer='word', stop_words=StopWords)),
+        ('lda', LatentDirichletAllocation(n_topics=j, n_jobs=-1)),
+        ('clf', LinearSVC(multi_class='ovr'))])
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1000, stratify=y)
+classifier = Pipeline([
+        ('vectorizer', CountVectorizer(max_features=n_feat, analyzer='word', stop_words=StopWords)),
+        ('lda', LatentDirichletAllocation(n_jobs=-1)),
+        ('clf', LinearSVC(multi_class='crammer-singer'))])
+params = {'lda__n_topics':[10,20,30,50,100,200],
+          'lda__doc_topic_prior':[None,0.001,0.01,0.1,1],
+          'lda__topic_word_prior':[None,0.001,0.01,0.1,1],
+          'clf__penalty':['l1','l2'],
+          'clf__C':[0.001,0.01,1,10,100]}
+
+gs = GridSearchCV(classifier, param_grid=params, cv=5, n_jobs=-1)
+gs.fit(X_train, y_train)
+gs.score(X_test, y_test)
+
+
+
+
+'''
 perplex = []
 for i in range(10):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y)
@@ -80,8 +148,35 @@ for i in range(10):
         x.append(lda.perplexity(vectorizer.fit_transform(X_test)))
     perplex.append(x)
         
-topic_perplex = pd.DataFrame(scores, columns=range(1000,10001,500))
-#plt.plot(topic_perplex.mean())
+topic_perplex = pd.DataFrame(perplex, columns=range(5,101,5))
+plt.plot(topic_perplex.mean())
+'''
+
+vectorizer = CountVectorizer(max_features=8000, analyzer='word', stop_words=StopWords)
+v = vectorizer.fit_transform(X_train)
+
+lda = LatentDirichletAllocation(n_topics=20, n_jobs=-1)
+lda.fit(v)
+
+lda.components_
+feat_names = vectorizer.get_feature_names()
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print "Topic %d:" % (topic_idx)
+        print " ".join([feature_names[i]
+                        for i in topic.argsort()[:-no_top_words - 1:-1]])
+
+display_topics(lda, feat_names, 20)
+
+
+
+
+
+
+
+
+
 
 
 
